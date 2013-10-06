464bd2fc698c0a29d32a6e485894feea8561498f
grigio <grigio.org@gmail.com>
2013-10-07 01:21:09 +0200

    FIX firefox menu closed bug

--- client/views/menu.js
+++ client/views/menu.js
@@ -1,29 +1,6 @@
 // from pure-css site ui js
 
 Meteor.startup(function () {
-
-  var menu = document.getElementById('menu'),
-      menuLink = document.getElementById('menuLink'),
-      layout = document.getElementById('layout');
-
-    toggleClass = function (element, className) {
-        var classes = element.className.split(/\s+/),
-            length = classes.length,
-            i = 0;
-
-        for(; i < length; i++) {
-          if (classes[i] === className) {
-            classes.splice(i, 1);
-            break;
-          }
-        }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
-
-        element.className = classes.join(' ');
-    };
   
 });
 
@@ -36,9 +13,8 @@ Template.menu.rendered = function() {
 Template.menu.events = {
   'click #menuLink' : function (e) {
     e.preventDefault();
-    var active = 'active';
-    toggleClass(layout, active);
-    toggleClass(menu, active);
+    $('#menu').toggleClass('active');
+    $('#layout').toggleClass('active');
   },
   'click .js-link' : function (e, t) { // all js-link
     Meteor.Router.to(e.target.getAttribute('href'));
